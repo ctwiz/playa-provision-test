e6aad68ffeb50a051265884a1b6953b57dfb9fa6
Dan Gailey <ournive@gmail.com>
2016-01-12 01:51:04 -0800

    params hash

--- hello.rb
+++ hello.rb
@@ -2,12 +2,33 @@ require 'sinatra'
 require 'json'
 
 
-use Rack::Auth::Basic do |username, password|
+
+use SimpleAuth do |username, password|
   username == 'playa' and password == 'test1234'
 end
 
 get '/playa/resource' do
   content_type :json
-  result = { id: 'id-1234-8888', config: {PLAYA_RESOURCE_URL: 'http://random_user:random_password@playa-provision-test.herokuapp.com/id-1234-8888' } }
+  id = 'id-1234-8888'
+  result = { id: id, config: {PLAYA_RESOURCE_URL: "http://random_user:random_password@playa-provision-test.herokuapp.com/#{id}" } }
   result.to_json
+end
+
+get '/playa/sso' do
+  params = request.env['rack.request.query_hash']
+end
+
+
+class SimpleAuth < Rack::Auth::Basic
+
+  def call(env)
+    request = Rack::Request.new(env)
+    case request.path
+    when '/playa/resource'
+      super
+    else
+      @app.call(env)
+    end
+  end
+
 end
