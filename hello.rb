af7e0c51d69d41561837c45878e2585a686ac5a2
Dan Gailey <ournive@gmail.com>
2016-01-11 20:57:20 -0800

    auth

--- hello.rb
+++ hello.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'json'
 
+
+use Rack::Auth::Basic do |username, password|
+  username == 'admin' and password == 'admin'
+end
+
 get '/playa/resource' do
   content_type :json
   { :key1 => 'value1', :key2 => 'value2' }.to_json
