e4bdf18210f3610eb3d0148f8fef123b85728e3d
Dan Gailey <ournive@gmail.com>
2016-01-17 17:52:36 -0800

    correct token delimiters

--- hello.rb
+++ hello.rb
@@ -36,7 +36,7 @@ post '/playa/sso' do
   # salt taken from tmp app
   salt = "d84973a6ed95a122495351e51bd2133d"
 
-  expected_token = params[:provision_id] + salt + params[:timestamp]
+  expected_token = params[:provision_id] + ":" + salt + ":" + params[:timestamp]
   expected_token = Digest::SHA1.hexdigest expected_token
 
   "Expected token: " + expected_token + "<br/> Received token: " + params[:token]
