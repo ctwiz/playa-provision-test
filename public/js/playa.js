31a929709ffb3ba93799c2b449e0523eabaf5e9f
Dan Gailey <ournive@gmail.com>
2016-01-17 22:51:39 -0800

    header js

--- public/js/playa.js
+++ public/js/playa.js
@@ -1,13 +1,13 @@
 window.onload = function() {
   // device name
-  var id = playa_getparam("agent_id");
-  var name = playa_getparam("agent_name");
+  var playa_id = playa_getparam("agent_id");
+  var playa_name = playa_getparam("agent_name");
+  var playa_url = playa_getparam("agent_url");
   // back link
   var playa = document.createElement('div');
   playa.style.backgroundColor = "#eceff0";
-  playa.style.padding = "10px";
   playa.style.width = "100%";
-  playa.innerHTML = '<div><a style="text-decoration: none;" href="http://getplaya.com/">Playa</a> &#187; <a style="text-decoration: none;" href="http://getplaya.com/device/'+id+'">'+name+'</a></div><div style="clear: both;">';
+  playa.innerHTML = '<div style="padding: 10px;"><a style="text-decoration: none;" href="http://getplaya.com/">Playa</a> &#187; <a style="text-decoration: none;" href="'+playa_url+'">'+playa_name+'</a></div><div style="clear: both;">';
   body = document.getElementsByTagName("body")[0];
   first_child = body.firstChild;
   body.insertBefore(playa, first_child);
